f7a5f2f5aca53baf04c80add307241432af63918
Pantkowsky <a.pantkowski@gmail.com>
2021-03-03 19:23:22 +0100

    extended `zio.App` and refactored the code accordingly

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,23 +1,20 @@
 package com.stocker.api
 
-import com.stocker.api.client.PolygonClient
-import zio.console.{getStrLn, putStrLn}
-import zio.{ExitCode, URIO}
+import com.stocker.api.client.Client
+import zio.console.{getStrLn, putStr, putStrLn}
+import zio.{ExitCode, URIO, ZIO}
 
 object Main extends zio.App {
 
-//  val ticker = "tsla"
-//  // response.body: by default read into an Either[String, String] to indicate failure or success
-//  println(PolygonClient.fetchDetailsForTicker(ticker))
+  val ticker = "tsla"
 
-  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
-    logic.exitCode
+  override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, ExitCode] =
+    logic.provideCustomLayer(Client.live.orDie).exitCode
 
   val logic =
     for {
-      _     <- putStrLn("Hello")
-      name  <- getStrLn
-      _     <- putStrLn(s"Hello ${name}")
+      result <- Client.fetch(ticker)
+      _ <- putStr(result.noSpaces)
     } yield()
 
 }
