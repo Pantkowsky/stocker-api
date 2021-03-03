d07fdd9c107e9bbdf9465d29e4f9e07950bbc635
Pantkowsky <a.pantkowski@gmail.com>
2021-03-03 19:23:02 +0100

    implemented `Client` based on zio

--- src/main/scala/client/Client.scala
+++ src/main/scala/client/Client.scala
@@ -1,28 +1,20 @@
 package com.stocker.api
 package client
 
+import secrets.Keys
+
+import io.circe.Json
 import sttp.client.asynchttpclient.WebSocketHandler
 import sttp.client.asynchttpclient.zio.AsyncHttpClientZioBackend
 import sttp.client.circe.SttpCirceApi
 import sttp.client.logging.slf4j.{Slf4jCurlBackend, Slf4jLoggingBackend}
 import sttp.client.{SttpApi, SttpBackend}
-import zio.{Has, Task, ZIO, ZLayer}
+import zio.{Has, IO, Task, ZIO, ZLayer}
 
-//trait Client {
-//
-//  def fetch(url: String) : zio.IO[String, String] = {
-//    val request = basicRequest.get(uri"$url")
-//
-//    val connection = HttpURLConnectionBackend()
-//    val response = request.send(connection)
-//    response.body match {
-//      case Left(s) => ZIO.fromEither(Left(s))
-//      case Right(s) => ZIO.fromEither(Right(s))
-//    }
-//  }
-//}
+object Client extends SttpApi with SttpCirceApi {
 
-object HttpClient extends SttpApi with SttpCirceApi {
+  private val apiKey = s"apiKey=${Keys.apiKey}"
+  private val baseUrl = s"https://api.polygon.io/v1/meta/symbols"
 
   type Backend = SttpBackend[Task, Nothing, WebSocketHandler]
   type ZioBackend = Has[Backend]
@@ -36,4 +28,29 @@ object HttpClient extends SttpApi with SttpCirceApi {
 
   val live: ZLayer[Any, Throwable, Has[Backend]] =
     createBackend.toLayer
+
+  def fetch(symbol: String) : ZIO[Has[Backend], Throwable, Json] = {
+
+    val url = buildUrl(symbol)
+
+    for {
+      backend <- ZIO.service[Backend]
+      response <- {
+        implicit val b: Backend = backend
+
+        // model classes go into asJson[Model]
+        basicRequest.get(uri"$url").response(asJson[Json]).send[Task]
+      }
+      parsedBody <- IO.fromEither(response.body)
+    } yield parsedBody
+  }
+
+  private def buildUrl(symbol: String) : String = {
+
+    val normalized = symbol.toUpperCase
+    val urlSuffix = s"/$normalized/company?&$apiKey"
+
+    baseUrl + urlSuffix
+  }
+
 }
